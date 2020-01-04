5e985b5e2f43d220ba16eade2424bd2299bea89e
edo shargal <edosh528@gmail.com>
2020-01-04 12:10:29 +0200

    speed update

--- speedTypingTest/script.js
+++ speedTypingTest/script.js
@@ -3,8 +3,28 @@ const qouteDisplay = document.getElementById("quoteDisplay")
 const qouteInput = document.getElementById("quoteInput")
 
 // Adding the event listener to the textarea element on input 
-document.addEventListener("input", function(){
-  console.log(qouteInput.value)
+qouteInput.addEventListener('input', () => {
+  const arrayQuote = qouteDisplay.querySelectorAll('span')
+  const arrayValue = qouteInput.value.split('')
+
+  let correct = true
+  arrayQuote.forEach((characterSpan, index) => {
+    const character = arrayValue[index]
+    if (character == null) {
+      characterSpan.classList.remove('correct')
+      characterSpan.classList.remove('incorrect')
+      correct = false
+    } else if (character === characterSpan.innerText) {
+      characterSpan.classList.add('correct')
+      characterSpan.classList.remove('incorrect')
+    } else {
+      characterSpan.classList.remove('correct')
+      characterSpan.classList.add('incorrect')
+      correct = false
+    }
+  })
+
+  if (correct) renderNewQuote()
 })
 
 //Getting qoute from api
@@ -17,13 +37,26 @@ async function getRandomQoute(){
 //Update new to qoute to DOM
 async function getNextQoute(){
   const qoute = await getRandomQoute()
-  console.log(qoute)
+  //console.log(qoute)
   qoute.split("").forEach(char => {
     // qouteDisplay.innerText = ""
     let charSpan = document.createElement("span")
     charSpan.innerText = char
     qouteDisplay.appendChild(charSpan)
   })
+}
+
+let startTime
+function startTimer() {
+  timerElement.innerText = 0
+  startTime = new Date()
+  setInterval(() => {
+    timer.innerText = getTimerTime()
+  }, 1000)
+}
+
+function getTimerTime() {
+  return Math.floor((new Date() - startTime) / 1000)
 }
 
 
